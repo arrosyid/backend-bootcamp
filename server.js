347874035a2f34415e74b5671370dcdbab70ffef
Moch. Iqbal Rosyidi <iqbal.rosyidi.32@gmail.com>
2025-03-04 12:36:44 +0700

    tambah 3 routes

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello, Backend!');
 });
+app.get('/tambah', (req, res) => {
+    res.send(20+10);
+});
+app.get('/kurang', (req, res) => {
+    res.send(20-10);
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
